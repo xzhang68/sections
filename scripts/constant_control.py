a0fd76a1cca6242ba876f9ea4a91637e419537ee
Xiaoming Zhang <xzhang68@stanford.edu>
2024-11-01 12:48:30 -0700

    section3

--- scripts/constant_control.py
+++ scripts/constant_control.py
@@ -2,28 +2,41 @@
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Bool
 
 
-class Publisher(Node):
+class ConstantControlTwist(Node):
     def __init__(self) -> None:
-        super().__init__("publisher_node")
-        self.get_logger().info('Publisher has been created...')
-        self.counter = 1
-        self.pub = self.create_publisher(String, '/cmd_vel', 10)
-        self.pub_timer = self.create_timer(0.2, self.publish)
-        
-    def publish(self) -> None:
-        msg = String()
-        msg.data = f"sending constant control… {self.counter}"
-        self.pub.publish(msg)
-        self.counter += 1
+        super().__init__('constant_control_twist')
+        self.get_logger().info('constant_control_twist node has been created...')
+        self.cmd_vel_pub = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.timer = self.create_timer(0.2, self.control_callback)
+        self.kill_sub = self.create_subscription(Bool, '/kill', self.kill_callback, 10)
+        self.running = True 
+
+    def control_callback(self) -> None:
+        if self.running:
+            msg = Twist()
+            msg.linear.x = 0.5
+            msg.angular.z = 0.0
+            self.cmd_vel_pub.publish(msg)
+            self.get_logger().info('sending constant control...')
+    
+    def kill_callback(self, msg: Bool) -> None:
+        if msg.data:
+            self.get_logger().warn('Kill command received! Stopping the robot.')
+            self.running = False
+            self.timer.cancel()
+            stop_msg = Twist()
+            self.cmd_vel_pub.publish(stop_msg)
 
 def main(args=None):
-    rclpy.init()        
-    node = Publisher()
+    rclpy.init()
+    node = ConstantControlTwist()
     rclpy.spin(node)
     rclpy.shutdown()
 
 if __name__ == "__main__":
     main()
+    
